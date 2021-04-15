789cc9230224721618f3aaa98c6a5183e54b108c
Tina Kovacova <tina@kovacova.ca>
2021-04-14 22:50:50 -0700

    add a basic ETL pipeline test

---
+++ ETL/transform_load.py
@@ -0,0 +1,19 @@
+import sqlite3
+import pandas as pd
+
+connection = sqlite3.connect('./recommendation.sqlite')
+
+get_course_rating = pd.read_sql_query(
+    "SELECT course_id, rating FROM ratings", connection)
+
+recommendPandas = (get_course_rating.groupby(['course_id']).mean())
+
+recommendPandasSorted = recommendPandas.sort_values(
+    by=['rating', 'course_id'], ascending=[False, False])
+
+print(recommendPandasSorted)
+
+recommendPandasSorted.to_sql(
+    'recommendPandas', connection, if_exists='replace')
+
+connection.close()
